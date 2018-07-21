7f9b6923b06ca236d363125296b1042509c143e8
XinruChen <tonytonytony0724@gmail.com>
2018-07-21 22:19:17 +1200

    made several sub pages.

--- my_portfolio/src/components/resume.js
+++ my_portfolio/src/components/resume.js
@@ -1,9 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Grid, Cell} from 'react-mdl';
 
 class Resume extends Component {
   render() {
     return (
-      <div><h1>Resume Page</h1></div>
+      <div>
+        <Grid>
+          <Cell col={4}>
+            <div style={{textAlign: "center"}}>
+              <img
+                src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+                alt="avatar"
+                style={{height: '200px'}}
+                />
+            </div>
+
+            <h2 style={{paddingTop: '2em'}}>Xinru Chen</h2>
+            <h4 style={{color: 'grey'}}>Programmer</h4>
+            <hr style={{borderTop: '3px solid #833fb2', width: "50%"}}/>
+              <p> type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets conta</p>
+            <hr style={{borderTop: '3px solid #833fb2', width: "50%"}}/>
+            <h5>Address</h5>
+            <p>114 Dunedas Street, Dunedin </p>
+            <h5>Phone</h5>
+            <p> (64) 275926380</p>
+            <h5>Email</h5>
+            <p> someone@example.com</p>
+            <h5>Web</h5>
+            <p> mywebsite.com</p>
+            <hr style={{borderTop: '3px solid #833fb2', width: "50%"}}/>
+          </Cell>
+          <Cell className="resume-right-col" col={8}>
+            <h2>Education</h2>
+          </Cell>
+        </Grid>
+      </div>
 
     )
   }
